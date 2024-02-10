9c4ba8717a1a0867e8a651af18267572532f6a1f
mark-elias <mark7elias@gmail.com>
2024-02-10 10:25:49 -0800

    initial commit

---
+++ react-to-do-list-project/src/ToDoList.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+
+function ToDoList() {
+  return (
+    <>
+      <h1>hello world</h1>
+      <ul>
+        <li>hi</li>
+        <li>hi</li>
+        <li>hi</li>
+        <li>hi</li>
+      </ul>
+    </>
+  );
+}
+
+export default ToDoList;
